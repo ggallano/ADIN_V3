292336d4189f831e03f6f1a3105af480019e1d6c
ANALOG\ggallan2 <glenn.gallano@analog.com>
2021-08-11 10:47:23 +0800

    Command Parameter is deleted in the xaml

--- ADIN1100-Eval/ViewModel/DeviceViewModel.cs
+++ ADIN1100-Eval/ViewModel/DeviceViewModel.cs
@@ -2685,10 +2685,11 @@ namespace ADIN1100_Eval.ViewModel
             {
                 if (obj is LocalLoopbackParameters)
                 {
-                    LocalLoopbackParameters localLoopbackParameters = (LocalLoopbackParameters)obj;
+                    LocalLoopbackParameters localLoopbackParameters = new LocalLoopbackParameters(); ;
                     localLoopbackParameters.gePhyLb_selt = (LoopBackMode)Enum.Parse(typeof(LoopBackMode), this.SelectedLoopbackItem.Name);
                     localLoopbackParameters.isolateRx_st = this.RxSuppression;
                     localLoopbackParameters.lbTxSup_st = this.TxSuppression;
+
                     if (this.selectedDevice != null)
                     {
                         try
