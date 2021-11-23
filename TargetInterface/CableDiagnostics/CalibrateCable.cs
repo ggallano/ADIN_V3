404343e8d6c6c6b48abdf340a68582806709cf7c
HJosafat <hanslaurence.josafat@analog.com>
2021-11-23 08:54:17 +0800

    - Reset Offset manual calibration value when entered an invalid input

--- TargetInterface/CableDiagnostics/CalibrateCable.cs
+++ TargetInterface/CableDiagnostics/CalibrateCable.cs
@@ -2,7 +2,13 @@
 {
     public class CalibrateCable
     {
+        /// <summary>
+        /// Nvp storage.
+        /// </summary>
         private float nvp;
+        /// <summary>
+        /// Gets or sets the Nvp.
+        /// </summary>
         public float NVP
         {
             get
@@ -18,8 +24,20 @@
                 }
             }
         }
+
+        /// <summary>
+        /// Gets or sets the Coeff0.
+        /// </summary>
         public float Coeff0 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Coeff1.
+        /// </summary>
         public float Coeffi { get; set; }
+
+        /// <summary>
+        /// Gets or sets the file name.
+        /// </summary>
         public string FileName { get; set; }
     }
 }
--- TargetInterface/CableDiagnostics/CalibrateOffset.cs
+++ TargetInterface/CableDiagnostics/CalibrateOffset.cs
@@ -2,7 +2,32 @@
 {
     public class CalibrateOffset
     {
-        public float Offset { get; set; }
+        /// <summary>
+        /// Offset.
+        /// </summary>
+        private float offset;
+        /// <summary>
+        /// Gets or sets the offset.
+        /// </summary>
+        public float Offset
+        {
+            get
+            {
+                return this.offset;
+            }
+
+            set
+            {
+                if (value >= 0.0f && value <= 1000.0f)
+                {
+                    this.offset = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the file name.
+        /// </summary>
         public string FileName { get; set; }
     }
 }
