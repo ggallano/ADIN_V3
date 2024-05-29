aca72c4fe35436040f9a7a47baaeea56b3d5b6a4
Glenn <glenn.gallano@analog.com>
2024-05-29 14:53:00 +0800

    ListBox Styling added

--- ADIN.Device/Services/ADIN1300FirmwareAPI.cs
+++ ADIN.Device/Services/ADIN1300FirmwareAPI.cs
@@ -136,12 +136,135 @@ namespace ADIN.Device.Services
 
         public List<string> LocalAdvertisedSpeedList()
         {
-            throw new NotImplementedException();
+            List<string> localSpeeds = new List<string>();
+
+            localSpeeds.Add(string.Empty);
+            localSpeeds.Add(string.Empty);
+
+            if(this.ReadYogaRg("Fd100Adv") == "1")
+            {
+                localSpeeds.Add("SPEED_100BASE_TX_FD_SPEED");
+            }
+            else
+            {
+                localSpeeds.Add(string.Empty);
+            }
+
+            if (this.ReadYogaRg("Hd100Adv") == "1")
+            {
+                localSpeeds.Add("SPEED_100BASE_TX_HD_SPEED");
+            }
+            else
+            {
+                localSpeeds.Add(string.Empty);
+            }
+            if (this.ReadYogaRg("Fd10Adv") == "1")
+            {
+                localSpeeds.Add("SPEED_10BASE_T_FD_SPEED");
+            }
+            else
+            {
+                localSpeeds.Add(string.Empty);
+            }
+
+            if (this.ReadYogaRg("Hd10Adv") == "1")
+            {
+                localSpeeds.Add("SPEED_10BASE_T_HD_SPEED");
+            }
+            else
+            {
+                localSpeeds.Add(string.Empty);
+            }
+
+            if (this.ReadYogaRg("EeeAdv") == "1")
+            {
+                localSpeeds.Add("SPEED_100BASE_EEE_SPEED");
+            }
+            else
+            {
+                localSpeeds.Add(string.Empty);
+            }
+
+            return localSpeeds;
         }
 
         public List<string> RemoteAdvertisedSpeedList()
         {
-            throw new NotImplementedException();
+            List<string> remoteSpeeds = new List<string>();
+
+            if (this.ReadYogaRg("LpFd1000Able") == "1")
+            {
+                remoteSpeeds.Add("SPEED_1000BASE_T_FD_SPEED");
+            }
+            else
+            {
+                remoteSpeeds.Add(string.Empty);
+            }
+
+            if (this.ReadYogaRg("LpHd1000Able") == "1")
+            {
+                remoteSpeeds.Add("SPEED_1000BASE_T_HD_SPEED");
+            }
+            else
+            {
+                remoteSpeeds.Add(string.Empty);
+            }
+
+            if (this.ReadYogaRg("LpFd100Able") == "1")
+            {
+                remoteSpeeds.Add("SPEED_100BASE_TX_FD_SPEED");
+            }
+            else
+            {
+                remoteSpeeds.Add(string.Empty);
+            }
+
+            if (this.ReadYogaRg("LpHd100Able") == "1")
+            {
+                remoteSpeeds.Add("SPEED_100BASE_TX_HD_SPEED");
+            }
+            else
+            {
+                remoteSpeeds.Add(string.Empty);
+            }
+
+            if (this.ReadYogaRg("LpFd10Able") == "1")
+            {
+                remoteSpeeds.Add("SPEED_10BASE_T_FD_SPEED");
+            }
+            else
+            {
+                remoteSpeeds.Add(string.Empty);
+            }
+
+            if (this.ReadYogaRg("LpHd10Able") == "1")
+            {
+                remoteSpeeds.Add("SPEED_10BASE_T_HD_SPEED");
+            }
+            else
+            {
+                remoteSpeeds.Add(string.Empty);
+            }
+
+            if (this.ReadYogaRg("LpEee1000Able") == "1")
+            {
+                remoteSpeeds.Add("SPEED_1000BASE_EEE_SPEED");
+            }
+            else
+            {
+                remoteSpeeds.Add(string.Empty);
+            }
+
+            if (this.ReadYogaRg("LpEee100Able") == "1")
+            {
+                remoteSpeeds.Add("SPEED_100BASE_EEE_SPEED");
+            }
+            else
+            {
+                remoteSpeeds.Add(string.Empty);
+            }
+
+            return remoteSpeeds;
         }
 
         public void DisableLinking(bool isDisabledLinking)
@@ -1180,7 +1303,28 @@ namespace ADIN.Device.Services
 
         public void ResetPhy(ResetType reset)
         {
-            throw new NotImplementedException();
+            switch (reset)
+            {
+                case ResetType.SubSysPin:
+                    WriteYodaRg("GeSftRstCfgEn", 1);
+                    WriteYodaRg("GeSftRst", 1);
+                    FeedbackLog("SubSys (Pin) reset", FeedbackType.Info);
+                    break;
+
+                case ResetType.SubSys:
+                    WriteYodaRg("GeSftRstCfgEn", 0);
+                    WriteYodaRg("GeSftRst", 1);
+                    FeedbackLog("SubSys reset", FeedbackType.Info);
+                    break;
+
+                case ResetType.Phy:
+                    WriteYodaRg("SftRst", 1);
+                    FeedbackLog("Phy reset", FeedbackType.Info);
+                    break;
+
+                default:
+                    break;
+            }
         }
     }
 }
