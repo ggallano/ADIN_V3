c585c951b84b6da12dbf288c0ce9e96ddc691878
ANALOG\JPerez32 <Johnsonralph.Perez@analog.com>
2024-06-11 16:39:31 +0800

    ADIN2111_Implementation: - removed calibration windows and diagram to separate components

---
+++ ADIN.WPF/Components/TDRCalibrationBoxHelpers.cs
@@ -0,0 +1,10 @@
+﻿using ADIN.WPF.Components;
+using System.Windows;
+using System.Windows.Input;
+
+internal static class TDRCalibrationBoxHelpers
+{
+
+    public static readonly DependencyProperty CalibrateCommandProperty =
+        DependencyProperty.Register("CalibrateCommand", typeof(ICommand), typeof(TDRCalibrationBox));
+}
