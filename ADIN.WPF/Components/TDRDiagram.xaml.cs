794df00cfce2a63dbcce79d813017c104e919f3a
ANALOG\JPerez32 <Johnsonralph.Perez@analog.com>
2024-06-13 14:50:37 +0800

    ADIN2111_Implementation: - added binding implementations for the TDR diagram

--- ADIN.WPF/Components/TDRDiagram.xaml.cs
+++ ADIN.WPF/Components/TDRDiagram.xaml.cs
@@ -24,5 +24,82 @@ namespace ADIN.WPF.Components
         {
             InitializeComponent();
         }
+
+        public static readonly DependencyProperty FaultDetectCommandProperty =
+            DependencyProperty.Register("FaultDetectCommand", typeof(ICommand), typeof(TDRDiagram));
+
+        public ICommand FaultDetectCommand
+        {
+            get
+            {
+                return (ICommand)GetValue(FaultDetectCommandProperty);
+            }
+            set
+            {
+                SetValue(FaultDetectCommandProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty IsFaultVisibilityProperty =
+            DependencyProperty.Register("IsFaultVisibility", typeof(bool), typeof(TDRDiagram));
+
+        public bool IsFaultVisibility
+        {
+            get
+            {
+                return (bool)GetValue(IsFaultVisibilityProperty);
+            }
+            set
+            {
+                SetValue(IsFaultVisibilityProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty DistToFaultProperty =
+            DependencyProperty.Register("DistToFault", typeof(string), typeof(TDRDiagram));
+
+        public string DistToFault
+        {
+            get
+            {
+                return (string)GetValue(FaultBackgroundBrushProperty);
+            }
+            set
+            {
+                SetValue(FaultBackgroundBrushProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty FaultBackgroundBrushProperty =
+            DependencyProperty.Register("FaultBackgroundBrush", typeof(Brush), typeof(TDRDiagram));
+
+        public Brush FaultBackgroundBrush
+        {
+            get
+            {
+                return (Brush)GetValue(FaultBackgroundBrushProperty);
+            }
+            set
+            {
+                SetValue(FaultBackgroundBrushProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty FaultStateProperty =
+            DependencyProperty.Register("FaultState", typeof(string), typeof(TDRDiagram));
+
+        public string FaultState
+        {
+            get
+            {
+                return (string)GetValue(FaultStateProperty);
+            }
+            set
+            {
+                SetValue(FaultStateProperty, value);
+            }
+        }
+
+
     }
 }
