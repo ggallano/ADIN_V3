59a3b5f788e3ed4e65c0bcf1d3a14d406d4f01a6
Glenn <glenn.gallano@analog.com>
2024-07-23 10:33:37 +0800

    version increment sci chart added in the list

--- ADIN.WPF/AutoVersionIncrement.cs
+++ ADIN.WPF/AutoVersionIncrement.cs
@@ -1,9 +1,9 @@
-﻿// <copyright file="ViewModelBase.cs" company="Analog Devices Inc.">
+﻿// <copyright file="AutoVersionIncrement.cs" company="Analog Devices Inc.">
 //     Copyright (c) 2024 Analog Devices Inc. All Rights Reserved.
 //     This software is proprietary and confidential to Analog Devices Inc. and its licensors.
 // </copyright>
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("3.5.912.206")]
+[assembly: AssemblyVersion("3.5.934.151")]
 
