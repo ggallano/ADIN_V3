f2c213e3a1c65ceb1882374c47100805229f058e
Glenn <glenn.gallano@analog.com>
2024-07-24 08:44:44 +0800

    foreground color added in the helper for consistent reference

---
+++ Helper/ForegroundColor/ForegroundColorStyle.cs
@@ -0,0 +1,31 @@
+﻿// <copyright file="ForegroundColorStyle.cs" company="Analog Devices Inc.">
+//     Copyright (c) 2024 Analog Devices Inc. All Rights Reserved.
+//     This software is proprietary and confidential to Analog Devices Inc. and its licensors.
+// </copyright>
+
+
+namespace Helper.ForegroundColor
+{
+    public static class ForegroundColorStyle
+    {
+        /// <summary>
+        /// hmc.color.gray.black
+        /// </summary>
+        public static string Black = "#101820";
+
+        /// <summary>
+        /// hmc.color.green.500
+        /// </summary>
+        public static string Green = "#2E9E6F";
+
+        /// <summary>
+        /// hmc.color.orange.500
+        /// </summary>
+        public static string Orange = "#E76423";
+
+        /// <summary>
+        /// hmc.color.red.700
+        /// </summary>
+        public static string Red = "#C81A28";
+    }
+}
